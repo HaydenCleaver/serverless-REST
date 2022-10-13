0f4308f4dd11b65d1162594851d83d5eebe17744
Hayden Cleaver <rexcleaver90@gmail.com>
2022-10-13 04:37:34 +0000

    init commit

---
+++ handleGetOne/index.js
@@ -0,0 +1,26 @@
+const dynamoose = require('dynamoose');
+
+const personSchema = new dynamoose.Schema({
+  id: String,
+  name: String,
+});
+
+const personModel = dynamoose.model('people-db', personSchema);
+
+exports.handler = async(event) => {
+  
+  const response = {statusCode: null, body: null};
+
+  try {
+    
+    let personRecords = await personModel.query(event.queryStringParameters.id).exec();
+    response.statusCode = 200;
+    response.body = JSON.stringify(personRecords);
+
+  } catch (err) {
+    console.log(err);
+    response.statusCode = 500;
+    response.body = JSON.stringify(err.message);
+  }
+  return response;
+};
